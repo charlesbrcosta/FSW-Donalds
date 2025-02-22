11422e13b39f7c46b61fccd99f59fcbb5f21fbe4
Charles Bruno <charlesbrcosta@gmail.com>
2025-02-21 21:12:09 -0300

    feat: add initial cart context and cart sheet components~

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -9,6 +9,9 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
 
+import CartSheet from "../../components/cart-sheet";
+import { useCart } from "../../context/cart";
+
 interface ProductDetailsProps {
     product: Prisma.ProductGetPayload<{ 
         include: {
@@ -23,6 +26,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
+    const { toggleCart } = useCart();
     const [quantify, setQuantify] = useState<number>(1);
     const handleDescreaseQuantify = () => {
         setQuantify((prev) => {
@@ -35,69 +39,75 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
     const handleIncreaseQuantify = () => {
         setQuantify((prev) => prev +1)
     }
+    const handleAddToCart = () => {
+        toggleCart();
+    }
     return ( 
-        <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden roudend-t-3xl p-5">        
-            <div className="flex-auto overflow-hidden">
-                {/* Restaurante */}
-                <div className="flex items-center gap-1.5">
-                    <Image 
-                        src={product.restaurant.avatarImageUrl} 
-                        alt={product.restaurant.name} 
-                        width={16}
-                        height={16}
-                        className="rounded-full"
-                    />
-                    <p className="text-xs text-muted-foreground">
-                        {product.restaurant.name}
-                    </p>
-                </div>
-                {/* Nome do produto */}
-                <h2 className="mt-1 text-xl font-semibold">{product.name}</h2> 
-                {/* Preço e quantidade */}
-                <div className="mt-3 flex items-center justify-between">
-                    <h3 className="text-xl font-semibold">
-                        {formatCurrency(product.price)}
-                    </h3>
-                    <div className="flex items-center gap-3 text-center">
-                        <Button 
-                            variant="outline" 
-                            className="h-8 w-8 rounded-xl"
-                            onClick={handleDescreaseQuantify}
-                        >
-                            <ChevronLeftIcon />
-                        </Button>
-                        <p className="w-4">{quantify}</p>
-                        <Button 
-                            variant="destructive" 
-                            className="h-8 w-8 rounded-xl"
-                            onClick={handleIncreaseQuantify}
-                        >
-                            <ChevronRightIcon />
-                        </Button>
+        <>
+            <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden roudend-t-3xl p-5">        
+                <div className="flex-auto overflow-hidden">
+                    {/* Restaurante */}
+                    <div className="flex items-center gap-1.5">
+                        <Image 
+                            src={product.restaurant.avatarImageUrl} 
+                            alt={product.restaurant.name} 
+                            width={16}
+                            height={16}
+                            className="rounded-full"
+                        />
+                        <p className="text-xs text-muted-foreground">
+                            {product.restaurant.name}
+                        </p>
                     </div>
-                </div> 
-                <ScrollArea className="h-full">
-                    {/* Sobre */}
-                    <div className="mt-6 space-y-3">
-                        <h4 className="font-semibold">Sobre</h4>
-                        <p className="text-sm text-muted-foreground">{product.description}</p>
-                    </div>   
-                    {/* Ingredientes */}
-                    <div className="mt-6 space-y-3">
-                        <div className="flex items-center gap-1">
-                            <ChefHatIcon size={18}/>
-                            <h4 className="font-semibold">Ingredientes</h4>
+                    {/* Nome do produto */}
+                    <h2 className="mt-1 text-xl font-semibold">{product.name}</h2> 
+                    {/* Preço e quantidade */}
+                    <div className="mt-3 flex items-center justify-between">
+                        <h3 className="text-xl font-semibold">
+                            {formatCurrency(product.price)}
+                        </h3>
+                        <div className="flex items-center gap-3 text-center">
+                            <Button 
+                                variant="outline" 
+                                className="h-8 w-8 rounded-xl"
+                                onClick={handleDescreaseQuantify}
+                            >
+                                <ChevronLeftIcon />
+                            </Button>
+                            <p className="w-4">{quantify}</p>
+                            <Button 
+                                variant="destructive" 
+                                className="h-8 w-8 rounded-xl"
+                                onClick={handleIncreaseQuantify}
+                            >
+                                <ChevronRightIcon />
+                            </Button>
                         </div>
-                        <ul className="list-disc px-5 text-sm text-muted-foreground">
-                            {product.ingredients.map((ingrendient) => (
-                                <li key={ingrendient}>{ingrendient}</li>
-                            ))}
-                        </ul>
-                    </div>   
-                </ScrollArea>
+                    </div> 
+                    <ScrollArea className="h-full">
+                        {/* Sobre */}
+                        <div className="mt-6 space-y-3">
+                            <h4 className="font-semibold">Sobre</h4>
+                            <p className="text-sm text-muted-foreground">{product.description}</p>
+                        </div>   
+                        {/* Ingredientes */}
+                        <div className="mt-6 space-y-3">
+                            <div className="flex items-center gap-1">
+                                <ChefHatIcon size={18}/>
+                                <h4 className="font-semibold">Ingredientes</h4>
+                            </div>
+                            <ul className="list-disc px-5 text-sm text-muted-foreground">
+                                {product.ingredients.map((ingrendient) => (
+                                    <li key={ingrendient}>{ingrendient}</li>
+                                ))}
+                            </ul>
+                        </div>   
+                    </ScrollArea>
+                </div>
+                <Button className="w-full rounded-full" onClick={handleAddToCart}>Adicionar à sacola</Button>
             </div>
-            <Button className="w-full rounded-full">Adicionar à sacola</Button>
-        </div>
+            <CartSheet />
+        </>
      );
 }
  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import { CartProvider } from "./[slug]/menu/context/cart";
+
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
@@ -19,11 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;  
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${poppins} antialiased`}
-      >
-        {children}
+    <html lang="pt-br">
+      <body className={`${poppins} antialiased`}>
+        <CartProvider>
+          {children}
+        </CartProvider>
       </body>
     </html>
   );
